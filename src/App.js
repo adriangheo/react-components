a61a9a4ec9f696485dcecdee96ed329eba68be3c
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-24 07:52:52 +0300

    add custom component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import CustomComponent from './components/CustomComponent';
 
 function App() {
   return (
     <div className="App">
-      <div>Hello React!</div>
+      <CustomComponent/>
     </div>
   );
 }
---
+++ src/components/CustomComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CustomComponent() {
+  return (
+    <div>Hello React!</div>
+  )
+}
+
+export default CustomComponent
